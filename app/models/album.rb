d8fca750cc105607219f502bb369024a1b6d38a6
Matthew Ager <matthew.a.ager@googlemail.com>
2013-09-25 18:02:41 +0200

    added mp3_exists and to_delete params to MacRuby program and removed from Hub

--- app/models/album.rb
+++ app/models/album.rb
@@ -26,7 +26,8 @@ class Album < ActiveRecord::Base
   
   attr_accessible :title_original, :label_id, :title_english, :release_year, :artist_original, :publisher_id,
                   :artist_english, :disc_num, :disc_count, :cd_code, :live_album, :explicit_lyrics, :cover, :gender,
-                  :language_id, :compilation, :origin_id, :synopsis, :genre_ids, :cover, :tracks_attributes
+                  :language_id, :compilation, :origin_id, :synopsis, :genre_ids, :cover, :tracks_attributes,
+                  :to_delete, :mp3_exists
 
   accepts_nested_attributes_for :tracks
   
