0773e8059b980c57b8e684ac1ae81f05f05dfd21
Matthew Ager <matthew.a.ager@googlemail.com>
2013-10-23 13:22:45 +0200

    Changed from cookie_store to active_record_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Iim::Application.config.session_store :cookie_store, key: '_iim_session'
+Iim::Application.config.session_store :active_record_store
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
